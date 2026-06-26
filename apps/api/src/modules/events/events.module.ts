bf1389166749e0b1271a570400e066d9c564d3e4
titusflorin27-web <titusflorin27@gmail.com>
2026-06-26 19:56:03 +0300

    build 003 wire events auth

--- apps/api/src/modules/events/events.module.ts
+++ apps/api/src/modules/events/events.module.ts
@@ -1,8 +1,11 @@
 import { Module } from "@nestjs/common";
+import { PrismaModule } from "../../common/prisma.module";
+import { AuthModule } from "../auth/auth.module";
 import { EventsController } from "./events.controller";
 import { EventsService } from "./events.service";
 
 @Module({
+  imports: [AuthModule, PrismaModule],
   controllers: [EventsController],
   providers: [EventsService],
 })
