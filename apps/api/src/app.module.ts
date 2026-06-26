9ff025f903ad754f3be6ad49e959bd508ae7d28f
titusflorin27-web <titusflorin27@gmail.com>
2026-06-26 19:54:42 +0300

    build 003 wire auth module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,21 +1,23 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
+import { PrismaModule } from "./common/prisma.module";
+import { AuthModule } from "./modules/auth/auth.module";
 import { HealthModule } from "./modules/health/health.module";
 import { OrganizationsModule } from "./modules/organizations/organizations.module";
 import { UsersModule } from "./modules/users/users.module";
 import { EventsModule } from "./modules/events/events.module";
 import { BusinessDnaModule } from "./modules/business-dna/business-dna.module";
-import { PrismaService } from "./common/prisma.service";
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    AuthModule,
     HealthModule,
     OrganizationsModule,
     UsersModule,
     EventsModule,
     BusinessDnaModule,
   ],
-  providers: [PrismaService],
 })
 export class AppModule {}
