f95f54b42ba564f0aa1f9739bca2e254b04e88ad
titusflorin27-web <titusflorin27@gmail.com>
2026-06-27 10:45:34 +0300

    build 019 add launch page

---
+++ apps/web/src/app/launch/page.tsx
@@ -0,0 +1,9 @@
+import { LaunchClient } from "./LaunchClient";
+
+export default function LaunchPage() {
+  return (
+    <main className="container page-stack">
+      <LaunchClient />
+    </main>
+  );
+}
