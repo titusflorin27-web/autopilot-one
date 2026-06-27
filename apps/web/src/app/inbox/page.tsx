50b6cac7fe012a92120a9e0afa09abe54fdcb46c
titusflorin27-web <titusflorin27@gmail.com>
2026-06-27 09:58:59 +0300

    build 015 add inbox page

---
+++ apps/web/src/app/inbox/page.tsx
@@ -0,0 +1,9 @@
+import { InboxClient } from "./InboxClient";
+
+export default function InboxPage() {
+  return (
+    <main className="container page-stack">
+      <InboxClient />
+    </main>
+  );
+}
