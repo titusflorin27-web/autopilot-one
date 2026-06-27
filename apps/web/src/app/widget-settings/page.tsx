9a84e884030aa451b4eb059c98b59532f3f2e5a2
titusflorin27-web <titusflorin27@gmail.com>
2026-06-27 09:16:19 +0300

    build 012 add widget settings page

---
+++ apps/web/src/app/widget-settings/page.tsx
@@ -0,0 +1,9 @@
+import { WidgetSettingsClient } from "./WidgetSettingsClient";
+
+export default function WidgetSettingsPage() {
+  return (
+    <main className="container page-stack">
+      <WidgetSettingsClient />
+    </main>
+  );
+}
